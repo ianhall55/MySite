9e830920cdeb5c2bf146b6034111217614390f18
Ian Hall <halli2008@yahoo.com>
2016-12-29 17:15:35 -0500

    deleted unnecessary files

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-var path = require('path');
-module.exports = {
-  entry: {
-    app: ["./my_site.js"]
-  },
-  output: {
-    path: path.join(__dirname, 'js'),
-    publicPath: '/js/',
-    filename: 'bundle.js',
-    devtoolModuleFilenameTemplate: '[resourcePath]',
-    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
-  },
-  devtool: 'source-maps',
-};
+// var path = require('path');
+// module.exports = {
+//   entry: {
+//     app: ["./my_site.js"]
+//   },
+//   output: {
+//     path: path.join(__dirname, 'js'),
+//     publicPath: '/js/',
+//     filename: 'bundle.js',
+//     devtoolModuleFilenameTemplate: '[resourcePath]',
+//     devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+//   },
+//   devtool: 'source-maps',
+// };
